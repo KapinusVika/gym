1d8c3cfd2ab40d19a7c04d08ee815e575dff54d3
Виктория Капинус <viktoriakapinus@MacBook-Pro-Viktoria.local>
2020-11-01 20:31:01 +0300

    corrected

--- src/com/java/automation/lab/fall/kapinus/core22/domain/classes/Inventory.java
+++ src/com/java/automation/lab/fall/kapinus/core22/domain/classes/Inventory.java
@@ -1,10 +1,32 @@
 package com.java.automation.lab.fall.kapinus.core22.domain.classes;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Inventory {
-    HashMap listCardioEq = new HashMap();
-    HashMap listPowerEq = new HashMap();
+    Map<Integer, CardioEquipment> listCardioEq = new HashMap<>();
+    Map<Integer, PowerEquipment> listPowerEq = new HashMap<>();
+
+    public Inventory(Map<Integer, CardioEquipment> listCardioEq, Map<Integer, PowerEquipment> listPowerEq){
+        this.listCardioEq = listCardioEq;
+        this.listPowerEq = listPowerEq;
+    }
+
+    public Map<Integer, CardioEquipment> getListCardioEq() {
+        return listCardioEq;
+    }
+
+    public void setListCardioEq(Map<Integer, CardioEquipment> listCardioEq) {
+        this.listCardioEq = listCardioEq;
+    }
+
+    public Map<Integer, PowerEquipment> getListPowerEq() {
+        return listPowerEq;
+    }
+
+    public void setListPowerEq(Map<Integer, PowerEquipment> listPowerEq) {
+        this.listPowerEq = listPowerEq;
+    }
 
     public void addListCardioEq(int inventoryNum, CardioEquipment cardioEquipment){
         listCardioEq.put(inventoryNum, cardioEquipment);
